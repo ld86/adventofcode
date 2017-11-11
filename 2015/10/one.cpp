2f688012259a4a96f7e10cfd07a6f50523cabff6
Bogdan Melnik <teh.ld86@gmail.com>
2017-11-11 18:00:54 +0300

    2015/10

---
+++ 2015/10/one.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <set>
+
+std::string MakeTurn(const std::string& s) {
+	std::stringstream ss;
+
+	size_t count = 1;
+	for (size_t i = 1; i < s.length(); ++i) {
+		if (s[i - 1] != s[i]) {
+			ss << count << s[i - 1];
+			count = 0;
+		}
+		count++;
+	}
+
+	ss << count << s[s.length() - 1];
+
+	return ss.str();
+}
+
+int main() {
+	std::ifstream input{"input.txt"};
+	std::string line;
+
+	while (std::getline(input, line)) {
+		for (size_t i = 0; i < 40; ++i) {
+			line = MakeTurn(line);
+		}
+		break;
+	}
+
+	std::cout << line.length() << std::endl;
+}
