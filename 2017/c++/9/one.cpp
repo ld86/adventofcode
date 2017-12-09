886f37e9f336b1e74d734fe4d9270a3c08fa034b
Bogdan Melnik <teh.ld86@gmail.com>
2017-12-09 13:18:50 +0300

    2017/c++/9

---
+++ 2017/c++/9/one.cpp
@@ -0,0 +1,93 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <unordered_map>
+#include <stack>
+
+template<typename T>
+T FromString(const std::string&& s);
+
+template<>
+int FromString(const std::string&& s) {
+    return std::stoi(s);
+}
+
+template<>
+std::string FromString(const std::string&& s) {
+    return s;
+}
+
+template<typename T>
+std::vector<T> SplitBy(const std::string& s, const std::string& c) {
+    auto l = 0;
+    auto r = s.find(c);
+    std::vector<T> result;
+
+    while (r != std::string::npos) {
+        result.push_back(FromString<T>(s.substr(l, r - l)));
+        l = r + c.size();
+        r = s.find(c, l);
+    }
+
+    result.push_back(FromString<T>(s.substr(l)));
+    return result;
+}
+
+int GetScore(const std::string& line) {
+    std::stack<char> s;
+
+    int score = 0;
+
+    bool inGarbage = false;
+    bool skipNext = false;
+
+    for (const char& c : line) {
+        if (skipNext) {
+            skipNext = false;
+            continue;
+        }
+        switch (c) {
+            case '<':
+                inGarbage = true;
+                break;
+            case '>':
+                inGarbage = false;
+                break;
+            case '!':
+                skipNext = true;
+                break;
+            case ',':
+                if (inGarbage) {
+                    continue;
+                }
+                break;
+            case '{':
+                if (inGarbage) {
+                    continue;
+                }
+                s.push(c);
+                score += s.size();
+                break;
+            case '}':
+                if (inGarbage) {
+                    continue;
+                }
+                s.pop();
+                break;
+        }
+    }
+    return score;
+}
+
+int main() {
+    std::ifstream input{"input.txt"};
+    std::string line;
+
+
+    while (getline(input, line)) {
+        std::cout << GetScore(line) << std::endl;
+    }
+
+}
